c4a6b39d7c2afbe4f9829f71b2abc924686da173
Lucas Bernardo <49166263+lucasber80@users.noreply.github.com>
2022-07-21 12:48:34 -0300

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  public_key: 'a8256b355471695a0e30adf9b7532d76',
+  private_key: '3fac574ef98063cf65dd6316d7fd91711c44be3e',
+  base_url: 'http://gateway.marvel.com/v1/public/',
 };
